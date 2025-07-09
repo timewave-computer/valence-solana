42769a56ebafa8ee6cdf6120b585c8d608ba00f5
Sam Hart <sam@hxrts.com>
2025-07-09 03:07:32 +0200

    Replace submodules with local zero.nix clone on sam-solana branch

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
     nixpkgs.url = "nixpkgs/nixos-24.11";
     flake-parts.url = "github:hercules-ci/flake-parts";
     devshell.url = "github:numtide/devshell";
-    zero-nix.url = "path:./submodules/zero.nix";
+    zero-nix.url = "path:./zero.nix";
   };
 
   outputs = {
